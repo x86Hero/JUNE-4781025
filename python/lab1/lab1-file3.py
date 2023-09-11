7d9968c894849bdc07db7c0c47681a4aad7e5958
Ashley <ashley.lee@lendscape.com>
2023-09-11 13:48:07 +0100

    swaping calculations around, based on lab instructions

--- python/lab1/lab1-file3.py
+++ python/lab1/lab1-file3.py
@@ -5,7 +5,7 @@ length=int(input("Please enter the length of the rectangle: "))
 # get user input and cast to int, store in width variable
 width=int(input("Please enter the height of the rectangle: "))
 
-# print the area calculation using length and width (l*w)
-print("The area of this rectangle is", length * width)
 # print the perimeter calculation using length and width (2*(l+w))
 print("The perimeter of this rectangle is", 2*(length + width))
+# print the area calculation using length and width (l*w)
+print("The area of this rectangle is", length * width)
