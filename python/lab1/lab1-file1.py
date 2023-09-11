c197e65ec713fb60c09a75e6e0db23a0ec474a17
Ashley <ashley.lee@lendscape.com>
2023-09-11 12:27:38 +0100

    New lab directory. Adding exercise scripts.

---
+++ python/lab1/lab1-file1.py
@@ -0,0 +1,12 @@
+# 1 print
+print('Hello World!')
+
+# 2 Display a message using vars
+# string for name
+name="Ash"
+# int for age
+age=27
+# printing using ',' to concatenate string and int vars and print message
+print(name,'is',age,'years old')
+# converting age to string and using '+' to concat strings together. NOTE: have to add white space manually.
+print(name+'is '+str(age)+' years old')
