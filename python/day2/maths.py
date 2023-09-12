5ada7f4ba9d156ed52a78189e9b4cb2382f51790
Ashley <ashley.lee@lendscape.com>
2023-09-12 09:58:29 +0100

    day2 dir and adding maths operators .py

---
+++ python/day2/maths.py
@@ -0,0 +1,17 @@
+# operators
+
+# standard 
+# +
+# -
+# *
+# /
+
+# others
+# ** - power of
+# times the first number, by itself, by n times. n being the second number provided.
+print(3 ** 5)
+
+# % - modulo
+# returns the remainder
+print(14 % 10)
+
