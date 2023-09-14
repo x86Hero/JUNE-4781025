1ffbc070086bdc4db0ea3830d566feabc2aa76d1
Ashley <ashley.lee@lendscape.com>
2023-09-14 11:29:50 +0100

    removing project, uploading mini lab - installing pyodbc and run a basic query.

--- python/day4/testing_pyodbc.py
+++ python/day4/testing_pyodbc.py
@@ -1,5 +1,3 @@
-# importing and using pyodbc connect.
-
 #import modules
 import pyodbc
 #create sql server connection string
@@ -8,13 +6,14 @@ connectionString = r'DRIVER={ODBC Driver 13 for SQL Server};SERVER=.\SQLExpress;
 conn = pyodbc.connect(connectionString)
 #create a db cursor to execute commands
 cur = conn.cursor()
-#run a query using cursor execute and store in results
-result = cur.execute('SELECT * FROM company').fetchall()
+
+
+# queries
+for row in cur.tables(catalog="QAStore",schema="dbo"):
+    print(row.table_name)
+
+
 #close connection
 conn.close()
 #loop through results
-for row in result:
-    print(row)
-
-
 
