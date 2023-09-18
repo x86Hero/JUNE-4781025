a1949673c0acc4f787d436dd635ece34cdbfbedc
Ashley <ashley.lee@lendscape.com>
2023-09-18 10:36:19 +0100

    10/10 lint score for function definition.

--- python/day6/lab1/linting_score.py
+++ python/day6/lab1/linting_score.py
@@ -1,6 +1,10 @@
-def count(sequence, item): 
- y = 0
- for n in sequence: 
- if n == item: 
- y += 1
- return y 
+""" count function """
+
+def count(sequence, item):
+    """ code to execute the count """
+    var_c = 0
+    for var_c in sequence:
+        if var_c == item:
+            var_c += 1
+            return var_c
+        return None
