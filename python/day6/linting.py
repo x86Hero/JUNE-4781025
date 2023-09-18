983fd3440ddc204150132a6310af5046184eea26
Ashley <ashley.lee@lendscape.com>
2023-09-18 10:04:19 +0100

    linting comments

---
+++ python/day6/linting.py
@@ -0,0 +1,9 @@
+"""
+linting / linters - help write code in a way it should look
+when using 'pylint', it has an alphanumeric priority system - C0101, C0303 etc. C stands for convention.
+the warnings issues will be marked W0101 etc - these should be a priority fix. W stands for warning.
+then there are E0602 etc - these are the most important, these issues will fail the code run. E stands for Error.
+
+Pylinters will also score your code out of 10
+""" 
+
